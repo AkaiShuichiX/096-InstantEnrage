6cb8f423a7b30056d5ec050af2e257e55faf1afb
Thanit <88444937+Aquarty@users.noreply.github.com>
2024-04-14 00:59:31 +0700

    ENRAGEEEEEE YAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
    ENRAGEEEEEEEEEEEEEEEEEEE YAYAYAYAYAYYAY

---
+++ Config.cs
@@ -0,0 +1,11 @@
+﻿using Exiled.API.Interfaces;
+
+namespace Old096
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; }
+        public bool InstantRage { get; set; } = true;
+    }
+}
---
+++ Old096.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using Exiled.Events.EventArgs.Scp096;
+using UnityEngine;
+
+namespace Old096
+{
+    public class Old096Plugin : Plugin<Config>
+    {
+        public override string Name => "Old_096";
+        public override string Author => "";
+        public override Version Version => new Version(1, 0, 0);
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+            Exiled.Events.Handlers.Scp096.AddingTarget += OnAddingTarget;
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+            Exiled.Events.Handlers.Scp096.AddingTarget -= OnAddingTarget;
+        }
+
+        private void OnAddingTarget(AddingTargetEventArgs ev)
+        {
+            if (Config.InstantRage)
+            {
+                Debug.Log("SCP - 096 - Instant Rage activated!");
+                ev.Scp096.Enrage();
+            }
+        }
+    }
+}
